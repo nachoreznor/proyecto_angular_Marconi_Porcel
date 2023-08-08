fa095e84e50eb48e2c66e99649aca285cf592df3
Ignacio_Marconi <ignaciomarconi@abc.gob.ar>
2023-08-08 20:33:01 -0300

    sitio_dinamico_MARCONI_PORCEL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IntroComponent } from './intro/intro.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PieComponent } from './pie/pie.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    IntroComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    EquipoComponent,
+    ContactoComponent,
+    PieComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
